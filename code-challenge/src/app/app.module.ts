b539e9722e59f571cc9dfed94ef97750fd3640e5
fangske <tdpawiqhndh@gmail.com>
2018-12-04 14:05:58 +0100

    changing code to be more modular

--- code-challenge/src/app/app.module.ts
+++ code-challenge/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ListHeaderComponent } from './list-header/list-header.component';
+import { ListItemsComponent } from './list-items/list-items.component';
+import { ListFooterComponent } from './list-footer/list-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListHeaderComponent,
+    ListItemsComponent,
+    ListFooterComponent
   ],
   imports: [
     BrowserModule,
