3d5095c6b16d78f4f5c3f0ebec1765a4be300022
tomkim22@outlook.com <tdpawiqhndh@gmail.com>
2018-12-05 13:36:59 +0100

    commit

--- code-challenge/src/app/app.module.ts
+++ code-challenge/src/app/app.module.ts
@@ -4,17 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ListHeaderComponent } from './list-header/list-header.component';
-import { ListItemsComponent } from './list-items/list-items.component';
-import { ListFooterComponent } from './list-footer/list-footer.component';
 import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ListHeaderComponent,
-    ListItemsComponent,
-    ListFooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
