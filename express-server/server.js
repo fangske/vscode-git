777ac8fd8b28590a8f9eae99e85b0fef52f3286e
tomkim22@outlook.com <tdpawiqhndh@gmail.com>
2018-12-05 12:07:35 +0100

    asdf

--- express-server/server.js
+++ express-server/server.js
@@ -9,33 +9,6 @@ app.use(cors());
 
 // Dummy data hardcoded
 
-const myObj = {
-    "dealers": [
-        {
-            "dealerId": "GS0000563",
-            "legalName": "Daimler AG",
-            "address": {
-                "street": "Mercedesstrae 137 (Einfahrt ber Benzstrae, Tor 12, Geb. 131/2)",
-                "zipCode": "70327",
-                "city": "Stuttgart",
-                "countryIsoCode": "DE"
-            }
-        },
-        {
-            "dealerId": "GS0000897",
-            "legalName": "Daimler AG",
-            "address": {
-                "street": "Hallschlag 65",
-                "zipCode": "70376",
-                "city": "Stuttgart",
-                "district": "Bad Cannstatt",
-                "countryIsoCode": "DE"
-            }
-        }
-    ]
-}
-
-
 app.get('/', function(req, res){
     res.send('hello from server');
 })
