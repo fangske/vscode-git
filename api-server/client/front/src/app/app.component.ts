dba4847c92fa2e0bc33066260568bc4be7ed715f
fangske <tdpawiqhndh@gmail.com>
2018-12-05 06:15:21 +0100

    commit to master

--- api-server/client/front/src/app/app.component.ts
+++ api-server/client/front/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component }             from '@angular/core';
+//import { BASE_URL, API_VERSION } from './shared'; 
+import { LoopBackConfig }        from './shared/sdk';
+import { Account, AccessToken }  from './shared/sdk/models';
+import { AccountApi }            from './shared/sdk/services';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    constructor() {
+      LoopBackConfig.setBaseURL('http://127.0.0.1:3000');
+      LoopBackConfig.setApiVersion('api');
+    }
+  }
+
   title = 'front';
 }
